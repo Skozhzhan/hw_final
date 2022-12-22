d2e18f3b85624aee2dba14b2ca08ecb8075e3c62
Skozhzhan <106809865+Skozhzhan@users.noreply.github.com>
2022-12-22 20:12:03 +0300

    Задали массив и обернули методами

--- Program.cs
+++ Program.cs
@@ -9,3 +9,32 @@
 // [“Hello”, “2”, “world”, “:-)”] → [“2”, “:-)”]
 // [“1234”, “1567”, “-2”, “computer science”] → [“-2”]
 // [“Russia”, “Denmark”, “Kazan”] → []
+
+//Создание заданного массива
+
+string[] array = new string[] { "Hello", "2", "world", ":-)" };
+string[] arrayNew = new string[array.Length];
+
+void NewArray()
+{
+    int j = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3)
+        {
+            arrayNew[j] = array[i];
+            j++;
+        }
+    }
+}
+
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+NewArray();
+PrintArray(arrayNew);
